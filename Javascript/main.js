d6aa90f54c0f095d9bebcba6be6ee2420a4d762b
MiguelGuedes1 <miguelsilvaguedes1@gmail.com>
2023-04-28 00:59:03 +0100

    Variables and strings

---
+++ Javascript/main.js
@@ -0,0 +1,17 @@
+var n1=3,n2=8
+var n3 //3
+
+n3=n1
+
+n1=n2
+n2=n3
+
+
+console.log(n1,n2)
+
+var message="hello"
+var name ="Mike"
+
+console.log(`${message} ${name}`)
+console.log(message.length)
+
