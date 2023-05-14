3cee8dd49f6a4afbbd2acf4f5e34d39ca065908b
MiguelGuedes1 <miguelsilvaguedes1@gmail.com>
2023-05-14 16:43:36 +0100

    final adjust in first server

--- Newsletter-Project/app.js
+++ Newsletter-Project/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyparser = require("body-parser")
 const request = require("request") // este módulo simplifica o processo de enviar requisições HTTP/HTTPS e receber as respostas correspondentes em aplicações Node.js
+const https=require("https")
 
 const app = express()
 app.use(express.static("public")) // esta linha permite importar css e ficheiros não estáticos para o servidor
@@ -11,18 +12,54 @@ app.get("/",function(req,res){
 })
 
 app.post("/",function(req,res){
-    var primeironome=req.body.primeironome // recupera o valor do input "primeironome" enviado pelo formulário HTML
-    var segundonome=req.body.segundonome // recupera o valor do input "segundonome" enviado pelo formulário HTML
-    var email=req.body.email // recupera o valor do input "email" enviado pelo formulário HTML
+    const primeironome=req.body.primeironome // recupera o valor do input "primeironome" enviado pelo formulário HTML
+    const segundonome=req.body.segundonome // recupera o valor do input "segundonome" enviado pelo formulário HTML
+    const email=req.body.email // recupera o valor do input "email" enviado pelo formulário HTML
 
-    console.log(primeironome,segundonome,email) // exibe as informações recebidas no servidor
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME: primeironome,
+                    LNAME: segundonome,
+                }
+            }
+        ]
+    }
+
+    const jsonData= JSON.stringify(data)
+
+    const url= "https://us17.api.mailchimp.com/3.0/lists/794659bc5c"
+    const options={
+        method:"POST",
+        auth:"miguelguedes13:53dfc1167e490c1df4583afb4d606c77-us17"
+    }
+
+   const request= https.request(url,options,function(response){
+
+        if(response.statusCode===200){
+            res.sendFile(__dirname + "/sucess.html")
+        }
+        else{
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+        response.on("data",function(data){
+            console.log(JSON.parse(data))
+        })
+    })    
+
+        request.write(jsonData)
+        request.end()
 })
 
-app.listen(3006,function(){
+app.listen(process.env.PORT||3006,function(){
     console.log("Servidor esta a funcionar correctamente na porta 3006") // exibe uma mensagem no console quando o servidor é iniciado
 })
 
 
-// 900004d26a63a8b08d5495f170bb7247-us17     api key
+// 53dfc1167e490c1df4583afb4d606c77-us17    api key
 
 // 794659bc5c.        audience ID
